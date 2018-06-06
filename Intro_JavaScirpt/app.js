10158c20438c0917de839133965436de0bad3da1
Boyun Zou <zouboyun@bu.edu>
2018-06-06 13:09:56 -0700

    age calculator practice

--- Intro_JavaScirpt/app.js
+++ Intro_JavaScirpt/app.js
@@ -1,6 +1,11 @@
-var firstName = prompt("What is your first name?");
-var lastName = prompt("What is your last name?");
-var age = prompt("What is your age?");
+// var firstName = prompt("What is your first name?");
+// var lastName = prompt("What is your last name?");
+// var age = prompt("What is your age?");
 
-console.log("Your full name is " + firstName + " " + lastName);
-console.log("Your age is " + age); 
+// console.log("Your full name is " + firstName + " " + lastName);
+// console.log("Your age is " + age); 
+
+var age = prompt('What is your age?');
+
+var days = age * 365.25;
+alert(age + ' years is roughly ' + days + ' days')
