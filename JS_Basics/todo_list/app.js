c0200691037dcf036641d743dfa50c153466c8c2
Boyun Zou <zouboyun@bu.edu>
2018-06-07 13:34:21 -0700

    new version of to do list

--- JS_Basics/todo_list/app.js
+++ JS_Basics/todo_list/app.js
@@ -4,8 +4,17 @@ while (action !== 'quit') {
   if (action === 'new') {
     var item = prompt('Enter item to do.')
     todoList.push(item);
+    console.log('new item added to the list.');
   } else if (action === 'list') {
-    console.log(todoList);
+    todoList.forEach(function(item, index) {
+      console.log('**********');
+      console.log(index + ': ' + item);
+      console.log('**********');
+    })
+  } else if (action === 'delete') {
+    var item = prompt('Enter the item you want to delete');
+    // splice the array
+    todoList.splice(todoList.indexOf(item), 1);
   }
   action = prompt("What would you like to do?");
 }
