e51e08c605962865532ffa0f5d88fc2ece6b3af2
Boyun Zou <zouboyun@bu.edu>
2018-06-07 22:01:21 -0700

    used toggle instead of flipping boolean

--- Intro_DOM/color_toggle/app.js
+++ Intro_DOM/color_toggle/app.js
@@ -2,12 +2,19 @@ var clickMe = document.querySelector('button');
 var color = document.querySelector('body');
 var clicked = false;
 
+//  use click and a boolean
+
+// clickMe.addEventListener('click', function() {
+//   if (!clicked) {
+//     color.style.backgroundColor = 'purple';
+//   } else {
+//     color.style.backgroundColor = '';
+//   }
+//   clicked = !clicked;
+// })
+
+// use classList.toggle
+
 clickMe.addEventListener('click', function() {
-  if (clicked === false) {
-    color.style.backgroundColor = 'purple';
-    clicked = true;
-  } else {
-    color.style.backgroundColor = '';
-    clicked = false;
-  }
+  color.classList.toggle('purple');
 })
