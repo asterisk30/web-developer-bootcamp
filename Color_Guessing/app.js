1beb795918e2d91ba87c2fae664a79bfd34a9868
Boyun Zou <zouboyun@bu.edu>
2018-06-09 20:12:43 -0700

    added easy and hard mode

--- Color_Guessing/app.js
+++ Color_Guessing/app.js
@@ -1,18 +1,18 @@
+// define variables
 var colors;
 var winningColor;
+const squares = document.querySelectorAll('.square');
+const rgbDisplay = document.querySelector('.rgbDisplay');
+const result = document.querySelector('#result');
+const title = document.querySelector('#title');
+const resetBtn = document.querySelector('#reset');
+const easyBtn = document.querySelector('#easy');
+const hardBtn = document.querySelector('#hard');
 
-// select html elements
-var squares = document.querySelectorAll('.square');
-var rgbDisplay = document.querySelector('.rgbDisplay');
-var result = document.querySelector('#result');
-var title = document.querySelector('#title');
-var resetBtn = document.querySelector('#reset');
-
+// original page set up
 pageSetUp();
 
-
 // Add event listeners
-
 squares.forEach((item, index) => {
   // add click listeners
   item.addEventListener('click', () => {
@@ -28,20 +28,46 @@ squares.forEach((item, index) => {
         result.textContent = '';
       }, 1000)
     }
-  })
+  });
 })
 
-resetBtn.addEventListener('click', pageSetUp)
+resetBtn.addEventListener('click', pageSetUp);
 
-// page set up code block
-function pageSetUp() {
-  colors = generateCol(6);
-  winningColor = pickWin();
-  rgbDisplay.textContent = winningColor;
-  squares.forEach((item, index) => {
-    // assign original colors
-    item.style.backgroundColor = colors[index];
-  })
+easyBtn.addEventListener('click', () => {
+  hardBtn.classList.remove('selected');
+  easyBtn.classList.add('selected');
+  pageSetUp('easy');
+});
+
+hardBtn.addEventListener('click', () => {
+  easyBtn.classList.remove('selected');
+  hardBtn.classList.add('selected');
+  pageSetUp();
+});
+
+// page set up
+function pageSetUp(mode) {
+  title.style.backgroundColor = 'black';
+  if (mode === 'easy') {
+    colors = generateCol(3);
+    winningColor = pickWin();
+    rgbDisplay.textContent = winningColor;
+    for (var i = 0; i < 3; i++) {
+      squares[i].style.backgroundColor = colors[i];
+    }
+    for (var i = 3; i < 6; i++) {
+      squares[i].style.display = 'none';
+    }
+  } else {
+    colors = generateCol(6);
+    winningColor = pickWin();
+    rgbDisplay.textContent = winningColor;
+    squares.forEach((item, index) => {
+      // assign original colors
+      item.style.backgroundColor = colors[index];
+      item.style.display = 'block';
+    })
+  }
 }
 
 // change all square colors
@@ -53,21 +79,23 @@ function changeAllSquare(color) {
 
 // generate colors
 function generateCol(num) {
-  var colors = [];
+  let colors = [];
   for (var i = 0; i < num; i++) {
     colors.push(randomCol());
   }
   return colors;
 }
+
 // pick random number and set winning color
 function pickWin() {
-  var index = Math.floor(Math.random() * colors.length);
+  let index = Math.floor(Math.random() * colors.length);
   return colors[index];
 }
+
 // random color function
 function randomCol() {
-  var red = Math.floor(Math.random() * 256);
-  var green = Math.floor(Math.random() * 256);
-  var blue = Math.floor(Math.random() * 256);
+  let red = Math.floor(Math.random() * 256);
+  let green = Math.floor(Math.random() * 256);
+  let blue = Math.floor(Math.random() * 256);
   return `rgb(${red}, ${green}, ${blue})`;
 }
