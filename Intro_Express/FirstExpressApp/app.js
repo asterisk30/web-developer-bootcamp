e97d23706b910312550d173c42d0c3719548cdda
Boyun Zou <zouboyun@bu.edu>
2018-06-18 16:26:57 -0700

    practice using express

--- Intro_Express/FirstExpressApp/app.js
+++ Intro_Express/FirstExpressApp/app.js
@@ -16,6 +16,23 @@ app.get('/dog', function(req, res) {
   res.send('Hi there dog');
 });
 
+// route patterns
+app.get('/r/:something', function(req, res) {
+  res.send('welcome to something');
+  console.log(req.params);
+})
+
+// route pattern for further down items
+app.get('/r/:something/:somethingElse/:someMoreThings', function(req, res) {
+  res.send('you are at some more things now.');
+  console.log(req.params);
+})
+// "*" => "Hi star" -- * will direct you to any other route that is not specified yet
+app.get('*', function(req, res) {
+  res.send('Hi star');
+});
+
+
 // tell express to listen for requests (start server)
 app.listen(3000, function() {
   console.log('server has started.')
