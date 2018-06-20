bc9bb77623e246b283e150a7271fecc8b2098bbf
Boyun Zou <zouboyun@bu.edu>
2018-06-19 20:08:34 -0700

    yelpcamp project set up

---
+++ YelpCamp/app.js
@@ -0,0 +1,50 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+  res.render('landing');
+})
+
+var campground = [
+  {
+    name: 'Hill',
+    image: 'https://www.canadapost.ca/cpc/assets/cpc/uploads/illustrations/small/special-discounts.svg'
+  },
+  {
+    name:'Mountain',
+    image:'https://www.canadapost.ca/cpc/assets/cpc/uploads/illustrations/small/special-discounts.svg'
+  },
+  {
+    name: 'Forest',
+    image:'https://www.canadapost.ca/cpc/assets/cpc/uploads/illustrations/small/special-discounts.svg'
+  }
+];
+
+app.get('/campground', function(req, res) {
+  res.render('campground', {campground:campground});
+})
+
+app.post('/campground', function(req, res) {
+  // get data from form
+  // add data to campground array
+  // redirect to campground page
+  let name = req.body.name;
+  let img = req.body.image;
+  let newCamp = {
+    name: name,
+    image: img
+  }
+  campground.push(newCamp);
+  res.redirect('/campground');
+})
+
+app.get('/campgroundnew', function(req, res) {
+  res.render('newcamp');
+})
+
+app.listen(3000, function() {
+  console.log('server is up.')
+})
---
+++ YelpCamp/readme.md
@@ -0,0 +1,13 @@
+# YelpCamp
+
+## Landing page
+
+Landing page will have all the campgrounds listed
+
+## Campground
+
+Each Campground will have a name and an image
+
+## Layout
+
+Need to add header and footer and bootstrap styling
